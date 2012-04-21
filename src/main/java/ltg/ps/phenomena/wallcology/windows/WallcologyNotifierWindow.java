ef8af6e367e50e0ba809a9cd84079c3447a1df49
Alessandro Gnoli <tebemis@gmail.com>
2012-04-21 12:37:19 -0500

    Bug fixing in the notifier window.

--- src/main/java/ltg/ps/phenomena/wallcology/windows/WallcologyNotifierWindow.java
+++ src/main/java/ltg/ps/phenomena/wallcology/windows/WallcologyNotifierWindow.java
@@ -32,7 +32,7 @@ public class WallcologyNotifierWindow extends PhenomenaWindow {
 			return xml;
 		int bluebugs = w.getPopulation().get("blueBug_s1") + w.getPopulation().get("blueBug_s2") 
 				+ w.getPopulation().get("blueBug_s3") + w.getPopulation().get("blueBug_s4");
-		int greenbugs = w.getPopulation().get("greenBug_s1") + w.getPopulation().get("greenBug_s1");
+		int greenbugs = w.getPopulation().get("greenBug_s1") + w.getPopulation().get("greenBug_s2");
 		int predator = w.getPopulation().get("fuzzPredator_s1") + w.getPopulation().get("fuzzPredator_s2"); 
 		xml =	"<getCount reqId=\"" + reqId + "\" wall=\"" + wallId + "\" >" + 
 					"<temperature>" + w.getTemperature() + "</temperature>" +
